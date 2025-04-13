cc1fb6a01ef9bb116a6f72d182480e9aca2cb7b1
Surajit Rana <surajit1602@gmail.com>
2025-04-13 23:01:29 +0530

    Time: 1 ms (99.44%) | Memory: 46.3 MB (11.15%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n]; // SC: O(N)
+        int index = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (nums[i] != 0) {
+                result[index++] = nums[i];
+            }
+        }
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] = result[i];
+        }
+    }
+}
