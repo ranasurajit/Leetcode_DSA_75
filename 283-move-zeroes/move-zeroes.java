9a0ac1253d8a2d87eda99d87bbd61482b6a2398b
Surajit Rana <surajit1602@gmail.com>
2025-04-13 23:46:07 +0530

    Time: 2 ms (84.82%) | Memory: 45.8 MB (91.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
+     * Approach II : Two Pointers Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int idxZero = 0;
+        int idx = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (nums[i] == 0) {
+                idx++;
+            } else {
+                // swap elements at indices idx and idxZero
+                int temp = nums[idxZero];
+                nums[idxZero] = nums[idx];
+                nums[idx] = temp;
+                idxZero++;
+                idx++;
+            }
+        }
+    }
+
+    /**
      * Approach I : Two Pass Approach
      *
      * TC: O(2 x N) ~ O(N)
      * SC: O(N)
      */
-    public void moveZeroes(int[] nums) {
+    public void moveZeroesApproachI(int[] nums) {
         int n = nums.length;
         int[] result = new int[n]; // SC: O(N)
         int index = 0;
